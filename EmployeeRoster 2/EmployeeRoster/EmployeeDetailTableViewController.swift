3f308543c92e88bfdf11dc7d066335f1df943155
Srishtiii998 <srishti1405.be23@chitkara.edu.in>
2025-09-17 00:34:10 +0530

    Add files via upload

---
+++ EmployeeRoster 2/EmployeeRoster/EmployeeDetailTableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  EmployeeDetailTableViewController.swift
+//  EmployeeRoster
+//
+//  Created by Srishti on 15/09/25.
+//
+import UIKit
+
+protocol EmployeeDetailTableViewControllerDelegate: AnyObject {
+    func employeeDetailTableViewController(_ controller: EmployeeDetailTableViewController, didSave employee: Employee)
+}
+
+class EmployeeDetailTableViewController: UITableViewController, UITextFieldDelegate ,EmployeeTypeTableViewControllerDelegate {
+    
+    
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var dobLabel: UILabel!
+    @IBOutlet var employeeTypeLabel: UILabel!
+    @IBOutlet var saveBarButtonItem: UIBarButtonItem!
+    
+    @IBOutlet weak var dobDatePicker: UIDatePicker!
+    let birthdayCellIndexPath = IndexPath(row: 2, section: 0)
+        let dateLabelCellIndexPath = IndexPath(row: 1, section: 0)
+        
+        var isEditingBirthday: Bool = false {
+            didSet {
+                dobDatePicker.isHidden = !isEditingBirthday
+            }
+        }
+        
+        weak var delegate: EmployeeDetailTableViewControllerDelegate?
+        var employee: Employee?
+        
+        var employeeType: EmployeeType?
+        
+        let dateFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            
+            return dateFormatter
+        }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateView()
+        updateSaveButtonState()
+    }
+    
+    @IBAction func datePickerAction(_ sender: UIDatePicker) {
+        dobLabel.text = dateFormatter.string(from: dobDatePicker.date)
+    }
+    func updateView() {
+        if let employee = employee {
+                    navigationItem.title = employee.name
+                    nameTextField.text = employee.name
+                    
+                    dobLabel.text = dateFormatter.string(from: employee.dateOfBirth)
+                    dobLabel.textColor = .black
+                    employeeTypeLabel.text = employee.employeeType.description
+                    employeeTypeLabel.textColor = .black
+                } else {
+                    navigationItem.title = "New Employee"
+                }
+    }
+    
+    func updateSaveButtonState() {
+        let shouldEnableSaveButton = nameTextField.text?.isEmpty == false && dobLabel.text?.isEmpty == false && employeeTypeLabel.text?.isEmpty == false
+        saveBarButtonItem.isEnabled = shouldEnableSaveButton
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = nameTextField.text else {
+            return
+        }
+        
+        let employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
+        delegate?.employeeDetailTableViewController(self, didSave: employee)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        employee = nil
+    }
+
+    @IBAction func nameTextFieldDidChange(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    
+    func employeeTypeTableViewController(_ controller: EmployeeTypeTableViewController, didSelect employeeType: EmployeeType) {
+            self.employeeType = employeeType
+            employeeTypeLabel.text = employeeType.description
+            employeeTypeLabel.textColor = .black
+            updateSaveButtonState()
+        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "type" {
+            let destination = segue.destination as! EmployeeTypeTableViewController
+            destination.delegate = self
+            destination.employeeType = employeeType
+        }
+    }
+        
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+            if indexPath == dateLabelCellIndexPath {
+                isEditingBirthday.toggle()
+                dobLabel.textColor = .black
+                dobLabel.text = dateFormatter.string(from: dobDatePicker.date)
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            switch indexPath {
+            case birthdayCellIndexPath where isEditingBirthday == false:
+                return 0
+            default:
+                return UITableView.automaticDimension
+            }
+        }
+
+}
---
+++ EmployeeRoster 2/EmployeeRoster/EmployeeTypeTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  EmployeeTypeTableViewController.swift
+//  EmployeeRoster
+//
+//  Created by Srishti on 15/08/25.
+//
+
+import UIKit
+
+protocol EmployeeTypeTableViewControllerDelegate: class {
+    func employeeTypeTableViewController(_ controller: EmployeeTypeTableViewController,
+                                           didSelect employeeType: EmployeeType)
+}
+
+class EmployeeTypeTableViewController: UITableViewController {
+    
+    var employeeType: EmployeeType?
+        weak var delegate: EmployeeTypeTableViewControllerDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return EmployeeType.allCases.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeTypeCell", for: indexPath)
+                
+                let type = EmployeeType.allCases[indexPath.row]
+                cell.textLabel?.text = type.description
+                
+                if employeeType == type {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+                return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let employeeType = EmployeeType.allCases[indexPath.row]
+                self.employeeType = employeeType
+                delegate?.employeeTypeTableViewController(self, didSelect: employeeType)
+                tableView.reloadData()
+    }
+
+}
